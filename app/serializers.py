27414ba76d41c7987975863f2e61a69b5d424672
artyomzhibarev <artyom.zhibarev@yandex.ru>
2022-02-18 00:10:52 +0300

    Prepare to deploy2

--- app/serializers.py
+++ app/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'image', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
 
